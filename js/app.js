846cf0b1556b9c46cfa7d9e6dfb8863b70e1f0a3
André Moura <andre.benedicto@etec.sp.gov.br>
2020-02-07 23:47:15 -0200

    Criação da classe despesa

--- js/app.js
+++ js/app.js
@@ -1,3 +1,19 @@
+// Classe do objeto despesa
+class Despesa {
+	constructor(ano, mes, dia, tipo, descricao, valor) {
+		this.ano = ano
+		this.mes = mes
+		this.dia = dia
+		this.tipo = tipo
+		this.descricao = descricao
+		this.valor = valor
+	}
+}
+
+// Eventos da página
+document.getElementById('cadastrar').addEventListener('click', cadastrarDespesa, false)
+
+// Função para recuperar os dados do formulário de cadastro
 function cadastrarDespesa() {
 	let ano = document.getElementById('ano')
 	let mes = document.getElementById('mes')
@@ -6,5 +22,6 @@ function cadastrarDespesa() {
 	let descricao = document.getElementById('descricao')
 	let valor = document.getElementById('valor')
 
-	console.log(ano.value, mes.value, dia.value, tipo.value, descricao.value, valor.value)
+	let despesa = new Despesa(ano.value, mes.value, dia.value, tipo.value, descricao.value, valor.value)
+	console.log(despesa)
 }
